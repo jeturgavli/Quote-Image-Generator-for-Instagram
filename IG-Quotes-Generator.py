2f7a6fc3c02a2df525e2772e32f4641b496f84ee
KRON . <keornpatel5656@gmail.com>
2024-06-23 19:50:39 -0700

    update Some Python codes

--- IG-Quotes-Generator.py
+++ IG-Quotes-Generator.py
@@ -1,3 +1,4 @@
+import os
 from PIL import Image, ImageDraw, ImageFont
 import colorama
 from colorama import Fore, Back, Style
@@ -14,13 +15,16 @@ def get_text_color():
             print("Invalid color. Please try again.")
 
 def save_image(image, filename):
-    image.save(f'Quotes_Output/{filename}.jpg')
+    output_dir = 'Quotes_Output'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    image.save(f'{output_dir}/{filename}.jpg')
 
 def main():
-    template = input("Choose Background : ")
+    template = input("Choose Background: ")
     templateBg = Image.open(f'Backgrounds/{template}.jpg')
 
-    font_object =  ImageFont.truetype('Fonts/arial.ttf', 40 )
+    font_object = ImageFont.truetype('Fonts/arial.ttf', 40)
     drawing_object = ImageDraw.Draw(templateBg)
 
     Parmanet_BG = Image.open('Program Stuff/Img-2.png')
@@ -28,30 +32,31 @@ def main():
 
     texts = []
     for i in range(5):
-        texts.append(input(f"{i} line : "))
+        texts.append(input(f"{i+1} line: "))
 
     text_color = get_text_color()
 
-    #shadows
-    for i, text in enumerate(texts):
-        x = 122
-        y = 750 + 50 * i
-        drawing_object.text((x, y), text, fill=(0, 0, 0), font=font_object)
+    # Default text location
+    x = 120
+    y_start = 750
+    line_height = 50
 
+    # Add text
     for i, text in enumerate(texts):
-        x = 120
-        y = 750 + 50 * i
+        y = y_start + line_height * i
         drawing_object.text((x, y), text, fill=text_color, font=font_object)
+
+    # Preview the image
+    templateBg.show()
 
     image_name = input("Enter image name: ")
     save_image(templateBg, image_name)
 
-    repeat = input("Would you like to run again? (y/n) ").lower()
+    repeat = input("Would you like to run again? (y/n): ").lower()
     if repeat == "y":
         main()
     else:
         print("Exiting...")
-        exit()
 
 if __name__ == '__main__':
     main()
